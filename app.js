f579212b7e77f47a87717ab6c6edef27b61d637b
krys11 <krysdario1996@gmail.com>
2023-08-29 22:31:35 +0100

    all

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {});
+const app = express();
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  console.log("Midle....");
+  res.send(
+    "<html><body><form action='/product' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body></html>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("two Midle....");
+  res.send("<h1>Home Page</h1>");
+});
+
+app.listen(3000);
