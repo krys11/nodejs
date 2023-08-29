34fd9680f2bdb025ce7130c0e2109dd2bb0f00c3
krys11 <krysdario1996@gmail.com>
2023-08-29 22:38:45 +0100

    all

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/add-product", (req, res, next) => {
   console.log("Midle....");
